105b18d4653a9f021730849612592f6954ef4eb2
BraianEM <braiimarmet99@gmail.com>
2023-08-11 12:30:14 -0300

    arreglos de navbar y footer

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,25 +6,31 @@ import { BiSolidShoppingBags, BiSolidContact } from "react-icons/bi";
 
 const Navbar = () => {
 
+
+  function menutoggle() {
+    const menu = document.getElementById('menu')
+    menu.click()
+  }
+
   return (
 
     <nav className="bg-dbg-100  text-dtext-200">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/" className="flex items-center ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between py-4 mx-auto ">
+        <Link to="/" className="flex items-center ps-4">
           <img src={logo} className="h-full mr-3 transform hover:scale-110 hover:rotate-12" alt="Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-dtext-200">
             Misiotrónica
           </span>
         </Link>
-        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-dtext-200 rounded-lg md:hidden hover:bg-dbg-100 focus:outline-none focus:ring-2 focus:ring-dbg-200 " aria-controls="navbar-default" aria-expanded="false">
+        <button id="menu" data-collapse-toggle="navbar-default" type="button" className="me-4 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-dtext-200 rounded-lg md:hidden hover:bg-dbg-100 focus:outline-none focus:ring-2 focus:ring-dbg-200 " aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15" />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className=" md:flex md:gap-5">
-            <li className="flex items-center m-auto gap-1 p-1">
+        <div className="hidden w-full md:block md:w-auto relative md:static z-50 " id="navbar-default">
+          <ul className=" md:flex md:gap-5 absolute md:static py-4 justify-end w-full p-4 bg-dbg-100">
+            <li onClick={menutoggle} className="flex items-center m-auto gap-1 p-1 py-2">
               <IoMdHome className="text-dtext-200 text-xl " />
               <Link
                 to="/"
@@ -33,7 +39,7 @@ const Navbar = () => {
                 Home
               </Link>
             </li>
-            <li className="flex items-center m-auto gap-1 p-1">
+            <li onClick={menutoggle} className="flex items-center m-auto gap-1 p-1 py-2">
               <IoMdPeople className="text-dtext-200 text-xl " />
               <Link
                 to="/about"
@@ -42,7 +48,7 @@ const Navbar = () => {
                 About
               </Link>
             </li>
-            <li className="flex items-center m-auto gap-1 p-1">
+            <li onClick={menutoggle} className="flex items-center m-auto gap-1 p-1 py-2">
               <BiSolidShoppingBags className="text-dtext-200 text-xl " />
               <Link
                 to="/store"
@@ -51,7 +57,7 @@ const Navbar = () => {
                 Store
               </Link>
             </li>
-            <li className="flex  items-center m-auto gap-1 p-1">
+            <li onClick={menutoggle} className="flex  items-center m-auto gap-1 p-1 py-2">
               <BiSolidContact className="text-dtext-200 text-xl" />
               <Link
                 to="/contact"
